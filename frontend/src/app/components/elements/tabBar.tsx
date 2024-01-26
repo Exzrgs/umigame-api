f32d85251e178b0ef5eba04b639a3a7369ab5b17
Yurai-Kitadate <yuurai18@gmail.com>
2024-01-26 20:33:04 +0900

    fix gap

--- frontend/src/app/components/elements/tabBar.tsx
+++ frontend/src/app/components/elements/tabBar.tsx
@@ -12,7 +12,7 @@ export default function TabBar() {
             <div style={{ position: 'fixed', width: '80px', height: '1080px', top: 0, left: 0 }}>
                 <Rectangle color={Color.White} width={80} height={1080}></Rectangle>
             </div>
-            <VStack style={{ position: 'fixed', width: '80px', height: '1080px', top: 24, left: 0, zIndex: 150 }} align='top' gap={24}>
+            <VStack style={{ position: 'fixed', width: '80px', height: '1080px', top: 24, left: 0, zIndex: 150 }} align='top' gap={30}>
                 <ImageCrop width='64px' height='64px' image={Logo.src}></ImageCrop>
                 <Circle color={Color.Gray} size={50}></Circle>
                 <Circle color={Color.Gray} size={50}></Circle>
