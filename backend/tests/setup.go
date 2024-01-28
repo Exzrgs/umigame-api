bb4903b4a697a6e14f048c046ebc3e369844bc3f
Exzrgs <s.nakao0120@gmail.com>
2024-01-28 19:13:00 +0900

    problemのテスト追加

--- backend/tests/setup.go
+++ backend/tests/setup.go
@@ -7,13 +7,13 @@ import (
 func (t *Test) Setup() error {
 	err := cleanupDB(t.env)
 	if err != nil {
-		fmt.Println("cleanup:", err)
+		fmt.Println("cleanup:", err.Error())
 		return err
 	}
 
 	err = setupTestData(t.env)
 	if err != nil {
-		fmt.Println("setup:", err)
+		fmt.Println("setup:", err.Error())
 		return err
 	}
 
